80de3e8adab50cdd11b88efa38df86c7293a79a7
unknown <roysatyajit772912@gmail.com>
2022-10-14 20:08:48 +0530

    day2-portfolio

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ const skillsContent = document.getElementsByClassName('skills_content'),
 skillsHeader = document.querySelectorAll('.skills_header')
 
 
+
+
+
 function toggleSkills(){
     let itemClass = this.parentNode.className
     // for(i = 0 ; i < skillsContent.length; i++){
@@ -51,4 +54,30 @@ function toggleSkills(){
 
 skillsHeader.forEach(el => {
     el.addEventListener('click', toggleSkills)
+})
+
+
+
+
+////////////// Qualification TAbs //////////////////////////////////
+
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach( tab => {
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+
+        tab.classList.add('qualification_active')
+    })
 })
