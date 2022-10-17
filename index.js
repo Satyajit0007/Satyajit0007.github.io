3fd9deaab384aad03f322f1d09face5d7749ec02
unknown <roysatyajit772912@gmail.com>
2022-10-17 23:16:02 +0530

    day4

--- index.js
+++ index.js
@@ -81,3 +81,21 @@ tabs.forEach( tab => {
         tab.classList.add('qualification_active')
     })
 })
+
+
+
+////////////////Portfolio swiper js//////////////////
+
+let swiper = new Swiper(".portfolio_container", {
+    cssMode: true,
+    loop : true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable : true,
+    },
+
+  });
